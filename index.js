75101130146967e2181f8926f898e92e88266fa1
Kevin <maingockien01@gmail.com>
2021-11-03 09:28:42 -0500

    Adding HTML structure

---
+++ index.js
@@ -0,0 +1,11 @@
+//This JS file is for interaciton between sections in HTML files.
+
+let pop_ups = $(".pop-up");
+
+let funcClosePopUps = function () {
+    pop_ups.hide();
+}
+
+let pop_up_close_btn = $(".pop-up-close");
+
+pop_up_close_btn.on("click", funcClosePopUps);
