19ed9b5da9b366c9baeb69adef31919cd6c06d65
Kevin <maingockien01@gmail.com>
2021-11-11 23:53:47 -0600

    Add: Load data from file

---
+++ dataQueries.js
@@ -0,0 +1,31 @@
+function getTransactionsInDay (time) {
+    return {}
+}
+
+function getTransactionsInMonth (month) {
+
+}
+
+function getTransactionsInYear (year) {
+
+}
+
+function getCategories () {
+
+}
+
+function getTransactionsInCategories (categoryID, year, month, date) {
+
+}
+
+function addCategory (name, notes) {
+    return {
+        categoryID: 0,
+        name,
+        notes
+    }
+}
+
+function addTransaction (category, time, amount, notes) {
+    return {}
+}
