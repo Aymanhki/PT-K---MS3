823afb24879726fc61f36bcef655339b88870378
villanro15 <you@example.com>
2021-11-12 21:09:50 -0600

    starting year view calendar

--- calendar.js
+++ calendar.js
@@ -1,14 +1,21 @@
 // calendar.js
-
+//calendar element ids
 const CALENDAR_DAY_GRID_ID = "days-of-week";
 const CALENDAR_HEADER_YEAR_ID = "calendar-header-year";
 const CALENDAR_HEADER_MONTH_ID = "calendar-header-month";
+const CALENDAR_WEEK_HEADER = "week-labels";
 
+//calendar next and prev button ids
 const CALENDAR_HEADER_PREV_ID = "calendar-prev";
 const CALENDAR_HEADER_NEXT_ID = "calendar-next";
 
 const MONTH_START = 0;
 const MONTH_END = 11;
+
+
+//yearly and month view switch id
+const SWITCH_YM = "tog";
+
 
 //Global variables for controlling calendar
 let year;
@@ -20,6 +27,7 @@ let isMonthView = true;
 const MONTHS = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
 const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const DAY_HEADER = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 function daysInMonth(month, year) {
     let d = new Date(year, month + 1, 0)
@@ -40,6 +48,8 @@ function loadCalendarDays(month, year) {
     let prevMonthDays = daysInMonth(month-1, year)
     let nextMonthDays = daysInMonth(month+1, year)
 
+    setWeekHeaders();
+
     for (let i = 1; i <= dayOfWeek; i++) {
         let nonMonthDate = createNonMonthDay(prevMonthDays-dayOfWeek+i, month, year)
         daysGrid.appendChild(nonMonthDate)
@@ -59,6 +69,7 @@ function loadCalendarDays(month, year) {
 
 function clearCalendar() {
     document.getElementById(CALENDAR_DAY_GRID_ID).innerHTML = "";
+    document.getElementById(CALENDAR_WEEK_HEADER).innerHTML = "";
 }
 
 function createNonMonthDay(date, month, year) {
@@ -98,6 +109,23 @@ function setCalendarMonthViewHeader (month, year) {
     monthView.innerText = `${MONTHS[month]}`;
 }
 
+//added year view header
+function setCalendarYearViewHeader (month, year) {
+    let yearView = document.getElementById(CALENDAR_HEADER_YEAR_ID);
+    let monthView = document.getElementById(CALENDAR_HEADER_MONTH_ID);
+
+    yearView.innerText = `${year}`;
+    monthView.innerText = "";
+}
+
+function clearCalendarHeaders () {
+    let yearView = document.getElementById(CALENDAR_HEADER_YEAR_ID);
+    let monthView = document.getElementById(CALENDAR_HEADER_MONTH_ID);
+
+    yearView.innerText = "";
+    monthView.innerText = "";
+}
+
 function setHeaderArrowListener () {
     if (isMonthView) {
         let prevBtn = $(`#${CALENDAR_HEADER_PREV_ID}`);
@@ -109,23 +137,43 @@ function setHeaderArrowListener () {
 }
 
 function nextMonth () {
-    if (month == MONTH_END) {
-        month = MONTH_START;
+    let sw_ym = document.getElementById(SWITCH_YM);
+    if(sw_ym.checked) {
         year ++;
-    } else {
-        month ++;
+        clearCalendarHeaders();
+        setCalendarYearViewHeader(month, year);
     }
-    reloadCalendar();
+    else {
+        if (month == MONTH_END) {
+            month = MONTH_START;
+            year ++;
+        } else {
+            month ++;
+        }
+        reloadCalendar();
+    }
+    
+    
 }
 
 function prevMonth () {
-    if (month == MONTH_START) {
-        month = MONTH_END;
+    let sw_ym = document.getElementById(SWITCH_YM);
+    if(sw_ym.checked) {
         year --;
-    } else {
-        month --;
+        clearCalendarHeaders();
+        setCalendarYearViewHeader(month, year);
     }
-    reloadCalendar();
+    else {
+        if (month == MONTH_START) {
+            month = MONTH_END;
+            year --;
+        } else {
+            month --;
+        }
+        reloadCalendar();
+    }
+    
+    
 }
 
 function reloadCalendar () {
@@ -133,6 +181,16 @@ function reloadCalendar () {
     loadCalendarDays(month, year);
 }
 
+function setWeekHeaders () {
+    let week_grid = document.getElementById(CALENDAR_WEEK_HEADER);
+    for (let i = 0; i < 7; i++) {
+        var day = document.createElement("li")
+        let day_lab = DAY_HEADER[i];
+        day.innerHTML = `<span>${day_lab}</span>`
+        week_grid.appendChild(day)
+    }//for
+}
+//setWeekHeaders
 $(document).ready(function () {
     let currentDay = new Date();
 
@@ -145,3 +203,19 @@ $(document).ready(function () {
     setHeaderArrowListener();
     reloadCalendar ();
 })
+
+/* -------------------------------------------- the switch for the month and yearly view ---------------------------------- */
+function checkState() {
+    let sw_ym = document.getElementById(SWITCH_YM);
+    console.log(sw_ym.checked);
+    clearCalendarHeaders();
+
+    if(sw_ym.checked) {
+        clearCalendar();
+        setCalendarYearViewHeader(month, year);
+    }
+    else {
+        reloadCalendar();
+        setCalendarMonthViewHeader(month, year);
+    }
+}
