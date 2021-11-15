bbacaadc9ad03ae37832c354e67852e4e2367648
lecfinney <alec.gudjon@gmail.com>
2021-11-15 16:46:23 -0600

    trying to do buttons but I'm dumb

--- popups.js
+++ popups.js
@@ -2,17 +2,10 @@
 
 let pop_up_transaction = $(".pop-up");
 
-let transaction_open = $(".category");
-
-let transaction_close = $(".close");
-
 let funcOpenTransac = function () {
-    pop_up_transaction.open();
+    pop_up_transaction.show();
 }
 
 let funcCloseTransac = function () {
     pop_up_transaction.hide();
 }
-
-transaction_open.on("click", funcOpenTransac);
-transaction_close.on("click", funcCloseTransac);
