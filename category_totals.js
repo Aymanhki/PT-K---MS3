2639a182502ca1ac3f84e879bf1966d823ef3938
aymanhkias <agamyahk@umanitoba.ca>
2021-11-15 23:25:38 -0600

    It will now show last month vs this month, it still looks ugly.

--- category_totals.js
+++ category_totals.js
@@ -12,6 +12,11 @@ function getRandomColor()
     return 'rgb('+Math.random()*255+', '+Math.random()*255+', '+Math.random()*255+')';
 }
 
+function RGB_Linear_Shade(p,c) {
+    var i=parseInt,r=Math.round,[a,b,c,d]=c.split(","),P=p<0,t=P?0:255*p,P=P?1+p:1-p;
+    return"rgb"+(d?"a(":"(")+r(i(a[3]=="a"?a.slice(5):a.slice(4))*P+t)+","+r(i(b)*P+t)+","+r(i(c)*P+t)+(d?","+d:")");
+}
+
 function getCategoryTotals()
 {
 
@@ -31,10 +36,8 @@ function getCategoryTotals()
 
     categories.sort()
 
-    function RGB_Linear_Shade(p,c) {
-        var i=parseInt,r=Math.round,[a,b,c,d]=c.split(","),P=p<0,t=P?0:255*p,P=P?1+p:1-p;
-        return"rgb"+(d?"a(":"(")+r(i(a[3]=="a"?a.slice(5):a.slice(4))*P+t)+","+r(i(b)*P+t)+","+r(i(c)*P+t)+(d?","+d:")");
-    }
+
+
 
     for(let i=0; i<categories.length; i++)
     {
--- line_graph.js
+++ line_graph.js
@@ -13,12 +13,30 @@ function drawLine()
         }
 
         let monthTransactions = [];
-        console.log(data[0].Month);
+
         for(let i=0; i<data.length; i++)
         {
             if(data[i].Month === month)
             {
                 monthTransactions.push(data[i]);
+            }
+        }
+        let lastMonthTransactions = [];
+
+        for(let i=0; i<data.length; i++)
+        {
+            let lastMonth = 0;
+            if(month-1 === 0)
+            {
+                lastMonth = 12;
+            }
+            else
+            {
+                lastMonth = month-1;
+            }
+            if(data[i].Month === lastMonth)
+            {
+                lastMonthTransactions.push(data[i]);
             }
         }
 
@@ -29,118 +47,138 @@ function drawLine()
             amounts.push(monthTransactions[i].Amount);
         }
 
-        let foodAmounts = [];
+        let lastMonthAmounts = []
 
-        for(let i=0; i<monthTransactions.length; i++)
+        for(let i=0; i<lastMonthTransactions.length; i++)
         {
-            if(monthTransactions[i].Category === doughnutLabels[0])
-            {
-                foodAmounts.push(monthTransactions[i].Amount);
-            }
-
+            lastMonthAmounts.push(lastMonthTransactions[i].Amount);
         }
-
-        let personalAmounts = [];
-
-        for(let i=0; i<monthTransactions.length; i++)
-        {
-            if(monthTransactions[i].Category === doughnutLabels[1])
-            {
-                personalAmounts.push(monthTransactions[i].Amount);
-            }
-
-        }
-
-        let utilitiesAmounts = [];
-
-        for(let i=0; i<monthTransactions.length; i++)
-        {
-            if(monthTransactions[i].Category === doughnutLabels[2])
-            {
-                utilitiesAmounts.push(monthTransactions[i].Amount);
-            }
-
-        }
-
-        let entertainmentAmounts = [];
-
-        for(let i=0; i<monthTransactions.length; i++)
-        {
-            if(monthTransactions[i].Category === doughnutLabels[3])
-            {
-                entertainmentAmounts.push(monthTransactions[i].Amount);
-            }
-
-        }
-
-        let healthAmount = [];
-
-        for(let i=0; i<monthTransactions.length; i++)
-        {
-            if(monthTransactions[i].Category === doughnutLabels[4])
-            {
-                healthAmount.push(monthTransactions[i].Amount);
-            }
-
-        }
-
-        let accommodationAmounts = [];
-
-        for(let i=0; i<monthTransactions.length; i++)
-        {
-            if(monthTransactions[i].Category === doughnutLabels[5])
-            {
-                accommodationAmounts.push(monthTransactions[i].Amount);
-            }
-
-        }
+        //
+        // let foodAmounts = [];
+        //
+        // for(let i=0; i<monthTransactions.length; i++)
+        // {
+        //     if(monthTransactions[i].Category === doughnutLabels[0])
+        //     {
+        //         foodAmounts.push(monthTransactions[i].Amount);
+        //     }
+        //
+        // }
+        //
+        // let personalAmounts = [];
+        //
+        // for(let i=0; i<monthTransactions.length; i++)
+        // {
+        //     if(monthTransactions[i].Category === doughnutLabels[1])
+        //     {
+        //         personalAmounts.push(monthTransactions[i].Amount);
+        //     }
+        //
+        // }
+        //
+        // let utilitiesAmounts = [];
+        //
+        // for(let i=0; i<monthTransactions.length; i++)
+        // {
+        //     if(monthTransactions[i].Category === doughnutLabels[2])
+        //     {
+        //         utilitiesAmounts.push(monthTransactions[i].Amount);
+        //     }
+        //
+        // }
+        //
+        // let entertainmentAmounts = [];
+        //
+        // for(let i=0; i<monthTransactions.length; i++)
+        // {
+        //     if(monthTransactions[i].Category === doughnutLabels[3])
+        //     {
+        //         entertainmentAmounts.push(monthTransactions[i].Amount);
+        //     }
+        //
+        // }
+        //
+        // let healthAmount = [];
+        //
+        // for(let i=0; i<monthTransactions.length; i++)
+        // {
+        //     if(monthTransactions[i].Category === doughnutLabels[4])
+        //     {
+        //         healthAmount.push(monthTransactions[i].Amount);
+        //     }
+        //
+        // }
+        //
+        // let accommodationAmounts = [];
+        //
+        // for(let i=0; i<monthTransactions.length; i++)
+        // {
+        //     if(monthTransactions[i].Category === doughnutLabels[5])
+        //     {
+        //         accommodationAmounts.push(monthTransactions[i].Amount);
+        //     }
+        //
+        // }
 
         const monthData = {
             labels: labels,
             datasets:
                 [
-                {
-                    label: doughnutLabels[0],
-                    data: foodAmounts,
-                    borderColor: doughnutColors[0],
-                    backgroundColor: doughnutColors[0],
-                    fill: false
-                },
-                {
-                    label: doughnutLabels[1],
-                    data: personalAmounts,
-                    borderColor: doughnutColors[1],
-                    backgroundColor: doughnutColors[1],
-                    fill: false
-                },
-                {
-                    label: doughnutLabels[2],
-                    data: utilitiesAmounts,
-                    borderColor: doughnutColors[2],
-                    backgroundColor: doughnutColors[2],
-                    fill: false
-                },
-                {
-                    label: doughnutLabels[3],
-                    data: entertainmentAmounts,
-                    borderColor: doughnutColors[3],
-                    backgroundColor: doughnutColors[3],
-                    fill: false
-                },
-                {
-                    label: doughnutLabels[4],
-                    data: healthAmount,
-                    borderColor: doughnutColors[4],
-                    backgroundColor: doughnutColors[4],
-                    fill: false
-                },
-                {
-                    label: doughnutLabels[5],
-                    data: accommodationAmounts,
-                    borderColor: doughnutColors[5],
-                    backgroundColor: doughnutColors[5],
-                    fill: false
-                }
+                // {
+                //     label: doughnutLabels[0],
+                //     data: foodAmounts,
+                //     borderColor: doughnutColors[0],
+                //     backgroundColor: doughnutColors[0],
+                //     fill: false
+                // },
+                // {
+                //     label: doughnutLabels[1],
+                //     data: personalAmounts,
+                //     borderColor: doughnutColors[1],
+                //     backgroundColor: doughnutColors[1],
+                //     fill: false
+                // },
+                // {
+                //     label: doughnutLabels[2],
+                //     data: utilitiesAmounts,
+                //     borderColor: doughnutColors[2],
+                //     backgroundColor: doughnutColors[2],
+                //     fill: false
+                // },
+                // {
+                //     label: doughnutLabels[3],
+                //     data: entertainmentAmounts,
+                //     borderColor: doughnutColors[3],
+                //     backgroundColor: doughnutColors[3],
+                //     fill: false
+                // },
+                // {
+                //     label: doughnutLabels[4],
+                //     data: healthAmount,
+                //     borderColor: doughnutColors[4],
+                //     backgroundColor: doughnutColors[4],
+                //     fill: false
+                // },
+                // {
+                //     label: doughnutLabels[5],
+                //     data: accommodationAmounts,
+                //     borderColor: doughnutColors[5],
+                //     backgroundColor: doughnutColors[5],
+                //     fill: false
+                // }
+
+                    {
+                        label: "Last Month",
+                        data: lastMonthAmounts,
+                        backgroundColor: RGB_Linear_Shade(0.7, getRandomColor())
+                    },
+                    {
+                        label: "This Month",
+                        data: amounts,
+                        backgroundColor: RGB_Linear_Shade(0.7, getRandomColor())
+
+                    },
+
                 ]
         };
 
