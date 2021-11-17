cbd62c4b1453e26fd49444c4f8666e2c74a51181
Kevin <maingockien01@gmail.com>
2021-11-16 22:08:05 -0600

    Remove date in prev and next month in year view

--- calendar.js
+++ calendar.js
@@ -304,11 +304,11 @@ function createOneMonthGrid (month, year) {
     let prevMonthDays = daysInMonth(month-1, year)
     let nextMonthDays = daysInMonth(month+1, year)
 
-
-    for (let i = 1; i <= dayOfWeek; i++) {
-        let nonMonthDate = createNonMonthDay(prevMonthDays-dayOfWeek+i, month, year)
-        monthGrid.appendChild(nonMonthDate)
-    }
+    // Remove comment if you want to show dates of previous month in year view
+    // for (let i = 1; i <= dayOfWeek; i++) {
+    //     let nonMonthDate = createNonMonthDay(prevMonthDays-dayOfWeek+i, month, year)
+    //     monthGrid.appendChild(nonMonthDate)
+    // }
 
     for (let i = 1; i <= numDays; i++) {
         let currentMonthDate = createCurrentMonthDay(i, month + 1, year)
@@ -316,10 +316,11 @@ function createOneMonthGrid (month, year) {
     }
 
 
-    for (let i = 1; i <= 42 - (dayOfWeek) - numDays; i++) {
-        let nonMonthDate = createNonMonthDay(i, month + 2, year)
-        monthGrid.appendChild(nonMonthDate)
-    }
+    // Remove comment if you want to show dates of next month in year view
+    // for (let i = 1; i <= 42 - (dayOfWeek) - numDays; i++) {
+    //     let nonMonthDate = createNonMonthDay(i, month + 2, year)
+    //     monthGrid.appendChild(nonMonthDate)
+    // }
 
     //placing the "ol" element into the final "li" to be passed by the function
     monthFinal.appendChild(monthGrid)
