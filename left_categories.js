d9a5d55058cbe7bef1de690b6ba59998985db3d9
villanro15 <you@example.com>
2021-11-18 12:29:30 -0600

    scroll

--- left_categories.js
+++ left_categories.js
@@ -1,12 +1,10 @@
 
 $( window ).on( "load", function() {
+    let category = data.find(data => data.Month === month);
     let cats = categories.length ;
-    console.log(cats);
     for (let i = 0; i < cats; i++) {
         let cat_pop = document.getElementById(`catt${i+1}`);
         cat_pop.innerText = `${categories[i][0]}\n${categories[i][1].toFixed(2)}`
-
-        console.log(`${cat_pop.innerText}`);
     }
 
     
