00021dec421fe867dbbcdf8c696f73f2c1eacfc9
Kevin <maingockien01@gmail.com>
2021-11-22 22:44:46 -0600

    Fix finally

--- dataGenerator.js
+++ dataGenerator.js
@@ -35,24 +35,23 @@ function makeCategory (categoryID, name, icon, color, notes) {
 // Load data from files
 //--------------------------------
 
-function readTextFile(file, callback) {
-    // var rawFile = new XMLHttpRequest();
-    // rawFile.overrideMimeType("application/json");
-    // rawFile.open("GET", file, true);
-    // rawFile.onreadystatechange = function() {
-    //     if (rawFile.readyState === 4 && rawFile.status == "200") {
-    //         callback(rawFile.responseText);
-    //     }
-    // }
-    // rawFile.send();
+// function readTextFile(file, callback) {
+//     // var rawFile = new XMLHttpRequest();
+//     // rawFile.overrideMimeType("application/json");
+//     // rawFile.open("GET", file, true);
+//     // rawFile.onreadystatechange = function() {
+//     //     if (rawFile.readyState === 4 && rawFile.status == "200") {
+//     //         callback(rawFile.responseText);
+//     //     }
+//     // }
+//     // rawFile.send();
 
-    callback();
+//     callback();
 
-}
+// }
 
 $(document).ready(()=> {
-readTextFile("./FakeData.json", function(text)
-{
+
     //data = JSON.parse(text);
     data = fakeData;
     sanitize (data);
@@ -61,7 +60,6 @@ readTextFile("./FakeData.json", function(text)
     getCatPopTotals();
     drawDoughnut();
     drawLine();
-});
 
 })
 
